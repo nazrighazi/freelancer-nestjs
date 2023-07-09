7d979edccad3e2a399ed97363cb072b27b6d5da7
nazri ghazi <nazri.ghazi@celcomdigi.com>
2023-07-09 23:38:41 +0800

    add cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AtGuard } from './common/guards/at.guard';
 import { FreelancerModule } from './freelancer/freelancer.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [PrismaModule, AuthModule, FreelancerModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    FreelancerModule,
+    CacheModule.register({
+      ttl: 86400000,
+      max: 200,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: AtGuard }],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: AtGuard },
+    { provide: APP_INTERCEPTOR, useClass: CacheInterceptor },
+  ],
 })
 export class AppModule {}
